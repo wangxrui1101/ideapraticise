bb6184ffb28e237bf6315ee0ac9cc2c704405ba7
minglang <2503479359@qq.com>
2019-04-16 15:18:15 +0800

    extends_2

--- src/demo_0415/Manager.java
+++ src/demo_0415/Manager.java
@@ -1,4 +1,22 @@
 package demo_0415;
 
-public class Manager {
+public class Manager extends Employee{
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    public double getSalary() {
+        double basesalary;
+        basesalary=super.getSalary();
+        bonus=bonus+basesalary;
+        return bonus;
+
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
 }
--- src/demo_0415/ManagerTest.java
+++ src/demo_0415/ManagerTest.java
@@ -1,4 +1,22 @@
 package demo_0415;
 
+/*
+    *@version:04_16_extends
+    *@author:minglang
+ */
+
 public class ManagerTest {
+    public static void main(String[] args) {
+        Manager boss=new Manager("sunshine",9000,2019,8,9);
+        boss.setBonus(5000);
+        Employee[] staff=new Employee[3];
+        staff[0]=boss;
+        staff[1]=new Employee("ming",890,2019,8,5);
+        staff[2]=new Employee("lang",1900,209,6,15);
+        for(Employee e:staff){
+            System.out.println("NAME: "+e.getName()+" salary: "+e.getSalary()+" date "+e.getDate());
+        }
+
+
+    }
 }
